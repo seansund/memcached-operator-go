49ed012009b3b77c9a98645265af5e283d497ab1
Sean Sundberg <seansund@us.ibm.com>
2020-11-05 21:41:41 -0600

    Updates api/v1alpha1/memcached_types.go

--- api/v1alpha1/memcached_types.go
+++ api/v1alpha1/memcached_types.go
@@ -28,14 +28,14 @@ type MemcachedSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of Memcached. Edit Memcached_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	Size int32 `json:"size"`
 }
 
 // MemcachedStatus defines the observed state of Memcached
 type MemcachedStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	Nodes []string `json:"nodes"`
 }
 
 // +kubebuilder:object:root=true
